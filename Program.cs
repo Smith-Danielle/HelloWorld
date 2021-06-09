e1668bddef0de615f26e9d0724d31645dc94eb83
Danielle Smith <smith.ddms@gmail.com>
2021-06-09 11:05:56 -0500

    HelloWorld C# Exercise

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Nice to meet you {name}. What state do you live in?");
+            string place = Console.ReadLine();
+            Console.WriteLine($"How many years have you lived in {place}?");
+            int years = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Thanks for the info {name}. It's interesting to know that you have lived in {place} for {years} years. ");
+        }
+    }
+}
